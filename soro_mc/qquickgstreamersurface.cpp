f8e842726a9e99da6be5fc6a430e3894cc1cd04a
doublejinitials <doublejinitials@gmail.com>
2017-03-01 16:57:52 -0600

    Allow QQuickGStreamerSurface to have a custom background color

--- soro_mc/qquickgstreamersurface.cpp
+++ soro_mc/qquickgstreamersurface.cpp
@@ -7,14 +7,17 @@ namespace Soro {
 
 QQuickGStreamerSurface::QQuickGStreamerSurface()
 {
-
+    _backgroundColor = QColor("black");
 }
 
 void QQuickGStreamerSurface::paint(QPainter *painter)
 {
     QRect targetArea(0, 0, width(), height());
+    painter->fillRect(targetArea, QBrush(_backgroundColor));
+    if (!_sink.isNull()) {
     // This line will show a syntax error if QT_NO_KEYWORDS is not defined, but it will still compile
-    QGlib::emit<void>(_sink, "paint", (void *) painter, (qreal)0, (qreal)0, (qreal)width(), (qreal)height());
+        QGlib::emit<void>(_sink, "paint", (void *) painter, (qreal)0, (qreal)0, (qreal)width(), (qreal)height());
+    }
 }
 
 void QQuickGStreamerSurface::setSink(const QGst::ElementPtr &sink)
@@ -34,5 +37,14 @@ void QQuickGStreamerSurface::clearSink() {
 void QQuickGStreamerSurface::onUpdate() {
     update();
 }
+
+QColor QQuickGStreamerSurface::getBackgroundColor() const {
+    return _backgroundColor;
+}
+
+void QQuickGStreamerSurface::setBackgroundColor(QColor color) {
+    _backgroundColor = color;
+}
+
 
 } // namespace Soro
--- soro_mc/qquickgstreamersurface.h
+++ soro_mc/qquickgstreamersurface.h
@@ -2,6 +2,7 @@
 #define QQUICKGSTREAMERSURFACE_H
 
 #include <QQuickPaintedItem>
+#include <QColor>
 
 #include <Qt5GStreamer/QGst/Element>
 
@@ -10,16 +11,21 @@ namespace Soro {
 class QQuickGStreamerSurface : public QQuickPaintedItem
 {
     Q_OBJECT
+    Q_PROPERTY(QColor backgroundColor READ getBackgroundColor WRITE setBackgroundColor)
+
 public:
     QQuickGStreamerSurface();
 
     void setSink(const QGst::ElementPtr & sink);
     void clearSink();
+    void setBackgroundColor(QColor color);
+    QColor getBackgroundColor() const;
 
     void paint(QPainter *painter) override;
 
 private:
     QGst::ElementPtr _sink;
+    QColor _backgroundColor;
 
 private:
     void onUpdate();
