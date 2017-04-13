d843f2732ce3b991466b745316cdb955ba5eb7b8
doublejinitials <doublejinitials@gmail.com>
2017-04-13 14:45:41 -0500

    Add runtime-switchable hardware rendering to QQuickGStreamerSurface

--- soro_mc/qquickgstreamersurface.cpp
+++ soro_mc/qquickgstreamersurface.cpp
@@ -2,49 +2,82 @@
 #include <QPainter>
 #include <Qt5GStreamer/QGlib/Connect>
 #include <Qt5GStreamer/QGlib/Signal>
+#include <Qt5GStreamer/QGst/ElementFactory>
+#include <QGLContext>
 
 namespace Soro {
 
 QQuickGStreamerSurface::QQuickGStreamerSurface()
 {
-    _backgroundColor = QColor("black");
+    _useHardwareRendering = false;
 }
 
-void QQuickGStreamerSurface::paint(QPainter *painter)
+QQuickGStreamerSurface::~QQuickGStreamerSurface()
 {
-    QRect targetArea(0, 0, width(), height());
-    painter->fillRect(targetArea, QBrush(_backgroundColor));
-    if (!_sink.isNull()) {
-    // This line will show a syntax error if QT_NO_KEYWORDS is not defined, but it will still compile
-        QGlib::emit<void>(_sink, "paint", (void *) painter, (qreal)0, (qreal)0, (qreal)width(), (qreal)height());
-    }
-}
-
-void QQuickGStreamerSurface::setSink(const QGst::ElementPtr &sink)
-{
-    clearSink();
-    QGlib::connect(sink, "update", this, &QQuickGStreamerSurface::onUpdate);
-    _sink = QGst::ElementPtr(sink);
-}
-
-void QQuickGStreamerSurface::clearSink() {
-    if (!_sink.isNull()) {
-        QGlib::disconnect(_sink, "update", this, &QQuickGStreamerSurface::onUpdate);
+    if (!_sink.isNull())
+    {
+        _sink->setState(QGst::StateNull);
         _sink.clear();
     }
 }
 
-void QQuickGStreamerSurface::onUpdate() {
+void QQuickGStreamerSurface::paint(QPainter *painter)
+{
+    if (!_sink.isNull())
+    {
+        // This line will show a syntax error if QT_NO_KEYWORDS is not defined, but it will still compile
+        QGlib::emit<void>(_sink, "paint", (void *) painter, (qreal)0, (qreal)0, (qreal)width(), (qreal)height());
+    }
+}
+
+bool QQuickGStreamerSurface::getEnableHardwareRendering() const
+{
+    return _useHardwareRendering;
+}
+
+void QQuickGStreamerSurface::setEnableHardwareRendering(bool enableHardwareRendering)
+{
+    // Cannot change this property after sink has already been created
+    if (_sink.isNull()) {
+        _useHardwareRendering = enableHardwareRendering;
+    }
+}
+
+QGst::ElementPtr QQuickGStreamerSurface::videoSink()
+{
+    if (_sink.isNull())
+    {
+        if (_useHardwareRendering)
+        {
+            _sink = QGst::ElementFactory::make("qt5glvideosink");
+
+            if (!_sink.isNull())
+            {
+                _sink->setProperty("glcontext", (void*) QGLContext::currentContext());
+
+                if (_sink->setState(QGst::StateReady) != QGst::StateChangeSuccess)
+                {
+                    _sink.clear();
+                }
+            }
+        }
+        else
+        {
+            _sink = QGst::ElementFactory::make("qt5videosink");
+        }
+    }
+
+    if (!_sink.isNull())
+    {
+        QGlib::connect(_sink, "update", this, &QQuickGStreamerSurface::onUpdate);
+    }
+
+    return _sink;
+}
+
+void QQuickGStreamerSurface::onUpdate()
+{
     update();
 }
-
-QColor QQuickGStreamerSurface::getBackgroundColor() const {
-    return _backgroundColor;
-}
-
-void QQuickGStreamerSurface::setBackgroundColor(QColor color) {
-    _backgroundColor = color;
-}
-
 
 } // namespace Soro
